a753b2269d4d3ff96ea617ffaf2a2c9a57337c9d
Guido van de Riet <guidovdriet@gmail.com>
2018-05-08 21:42:13 +0200

    Update walking animation

--- dev/characters/Bomber.ts
+++ dev/characters/Bomber.ts
@@ -1,11 +1,11 @@
 /// <reference path="../GameObject.ts"/>
 
 class Bomber extends GameObject {
-
   private leftSpeed: number = 0;
   private upSpeed: number = 0;
   private downSpeed: number = 0;
   private rightSpeed: number = 0;
+  private animationCount: number = 0;
 
   constructor() {
     super();
@@ -13,24 +13,24 @@ class Bomber extends GameObject {
     this.div = document.createElement("bomber");
     document.body.appendChild(this.div);
 
-    this.startPosition();
+    this.start();
 
-    window.addEventListener('keydown', (event: KeyboardEvent) =>
-      this.move(event, 6)
-    );
+    window.addEventListener("keydown", (event: KeyboardEvent) => {
+      this.move(event, 6);
+    });
 
-    window.addEventListener('keyup', (event: KeyboardEvent) => 
+    window.addEventListener("keyup", (event: KeyboardEvent) =>
       this.move(event, 0)
     );
   }
 
-  private startPosition() {
+  private start() {
     this.x = 50;
     this.y = 100;
+    this.setWalkingBackground(true);
   }
 
   public move(event: KeyboardEvent, speed: number): void {
-    
     const leftKey: number = 37;
     const upKey: number = 38;
     const rightKey: number = 39;
@@ -39,17 +39,35 @@ class Bomber extends GameObject {
     switch (event.keyCode) {
       case leftKey:
         this.leftSpeed = speed;
+        this.setWalkingBackground(false);
         break;
       case rightKey:
         this.rightSpeed = speed;
+        this.setWalkingBackground(false);
         break;
       case upKey:
         this.upSpeed = speed;
+        this.setWalkingBackground(false);
         break;
       case downKey:
         this.downSpeed = speed;
+        this.setWalkingBackground(false);
         break;
     }
+  }
+
+  private setWalkingBackground(startPostion: boolean) {
+    const baseUrl = '../docs/img/characters/bomber/spr_player_';
+    if(startPostion) {
+      this.animationCount = 0;
+    } else {
+      this.animationCount <= 2
+        ? this.animationCount++
+        : (this.animationCount = 0);
+    }
+    this.div.style.backgroundImage = `url(${baseUrl}${
+      this.animationCount
+    }.png)`;
   }
 
   public update() {
@@ -63,5 +81,5 @@ class Bomber extends GameObject {
 
   private draw(): void {
     this.div.style.transform = `translate3d(${this.x}px, ${this.y}px, 0px)`;
-}
+  }
 }
--- docs/js/main.js
+++ docs/js/main.js
@@ -44,20 +44,22 @@ var Bomber = (function (_super) {
         _this.upSpeed = 0;
         _this.downSpeed = 0;
         _this.rightSpeed = 0;
+        _this.animationCount = 0;
         _this.div = document.createElement("bomber");
         document.body.appendChild(_this.div);
-        _this.startPosition();
-        window.addEventListener('keydown', function (event) {
-            return _this.move(event, 6);
+        _this.start();
+        window.addEventListener("keydown", function (event) {
+            _this.move(event, 6);
         });
-        window.addEventListener('keyup', function (event) {
+        window.addEventListener("keyup", function (event) {
             return _this.move(event, 0);
         });
         return _this;
     }
-    Bomber.prototype.startPosition = function () {
+    Bomber.prototype.start = function () {
         this.x = 50;
         this.y = 100;
+        this.setWalkingBackground(true);
     };
     Bomber.prototype.move = function (event, speed) {
         var leftKey = 37;
@@ -67,17 +69,33 @@ var Bomber = (function (_super) {
         switch (event.keyCode) {
             case leftKey:
                 this.leftSpeed = speed;
+                this.setWalkingBackground(false);
                 break;
             case rightKey:
                 this.rightSpeed = speed;
+                this.setWalkingBackground(false);
                 break;
             case upKey:
                 this.upSpeed = speed;
+                this.setWalkingBackground(false);
                 break;
             case downKey:
                 this.downSpeed = speed;
+                this.setWalkingBackground(false);
                 break;
         }
+    };
+    Bomber.prototype.setWalkingBackground = function (startPostion) {
+        var baseUrl = '../docs/img/characters/bomber/spr_player_';
+        if (startPostion) {
+            this.animationCount = 0;
+        }
+        else {
+            this.animationCount <= 2
+                ? this.animationCount++
+                : (this.animationCount = 0);
+        }
+        this.div.style.backgroundImage = "url(" + baseUrl + this.animationCount + ".png)";
     };
     Bomber.prototype.update = function () {
         var targetX = this.x - this.leftSpeed + this.rightSpeed;
